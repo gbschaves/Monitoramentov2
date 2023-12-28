79e45f37c99bae889ca797487c4ff4d214f42b3c
Guilherme Chaves <gb.chaves@hotmail.com>
2023-12-28 16:14:14 -0300

    fix casesensitive

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import DashboardComponent from 'DashboardComponent'
+import DashboardComponent from '../src/Components/Dashboard/Dashboardcomponent'
 import https from 'https';
 import { Doughnut, Bar } from 'react-chartjs-2';
 // Importe o Chart.js e suas escalas
